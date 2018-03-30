c713536988685b416dc17d4b8cb7e5b43b282e11
tatek <tatek.ashenafi@gmail.com>
2018-03-30 18:37:09 -0500

    the fist tool bar test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, ROUTES} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+                  AppComponent,
+                  ToolBarComponent,
+                  HomeComponent,
+                  AboutComponent
+                ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+              BrowserModule,
+              RouterModule.forRoot([
+                                      {path: '', component: HomeComponent},
+                                      {path: 'About', component: AboutComponent}
+                                    ])
+           ],
+  providers: [
+
+              ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
